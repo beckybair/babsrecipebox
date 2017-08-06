07f4eb39c1eaa2ce6fb7839cb3c48991449b88fc
beckybair <beckyhowen@gmail.com>
2017-08-06 10:44:29 -0500

    fixed bucket errors

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module RecipeBox
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.before_configuration do
+        env_file = File.join(Rails.root, 'config', 'application.yml')
+        YAML.load(File.open(env_file)).each do |key, value|
+            ENV[key.to_s] = value
+        end if File.exists?(env_file)
+    end
   end
 end
